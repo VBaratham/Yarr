5ff1721fe1565177352bc654bed5d102f823d03d
Vyassa Baratham <vbaratham@berkeley.edu>
2017-11-17 14:27:58 -0800

    untested read_rawdata_vb

--- src/scripts/vb_read_rawData.cxx
+++ src/scripts/vb_read_rawData.cxx
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 #include "TTree.h"
+#include "TFile.h"
 
 
 struct Fei4Hit {
@@ -11,38 +13,61 @@ struct Fei4Hit {
     unsigned unused : 11;
 };
 
-void eventFromFileBinary(std::fstream &handle,  uint16_t* bcid, uint16_t* l1id, uint32_t* tag) {
-    uint16_t t_hits = 0;
-    handle.read((char*)&tag, sizeof(uint32_t));
-    handle.read((char*)&l1id, sizeof(uint16_t));
-    handle.read((char*)&bcid, sizeof(uint16_t));
-    handle.read((char*)&t_hits, sizeof(uint16_t));
+void eventFromFileBinary(std::fstream &handle,  uint16_t* bcid, uint16_t* l1id,
+			 uint32_t* tag, vector<int> col, vector<int> row, vector<int> tot) {
+  uint16_t nhits;
+  col.clear();
+  row.clear();
+  tot.clear();
+  
+  handle.read((char*)tag, sizeof(uint32_t));
+  handle.read((char*)l1id, sizeof(uint16_t));
+  handle.read((char*)bcid, sizeof(uint16_t));
+  handle.read((char*)nhits, sizeof(uint16_t));
 
-    std::cout << bcid << ","
-              << l1id << ","
-              << tag << ","
-              << t_hits << std::endl;
-    
-    for (unsigned i=0; i<t_hits; i++) {
-        struct Fei4Hit hit;
-        handle.read((char*)&hit, sizeof(Fei4Hit));
-        std::cout << "\t"
-                  << hit.row << "," 
-                  << hit.col << "," 
-                  << hit.tot << std::endl;
-    }
+  for (unsigned i=0; i<nhits; i++) {
+    struct Fei4Hit hit;
+    handle.read((char*)&hit, sizeof(Fei4Hit));
+
+    col.push_back(hit.col);
+    row.push_back(hit.row);
+    tot.bush_back(hit.tot);
+  }
 }
 
 
 void vb_read_rawData(string fn){
-    std::fstream file(fn, std::fstream::in | std::fstream::binary);
 
-    file.seekg(0, std::ios_base::end);
-    int size = file.tellg();
-    file.seekg(0, std::ios_base::beg);
-    std::cout << "Size: " << size/1024.0/1024.0 << " MB" << std::endl;
+  // tree variables
+  uint16_t bcid, l1id;
+  uint32_t tag;
+  std::vector<int> row, col, tot;
+  int ONE = 1;
 
-    while(file) {
-      eventFromFileBinary(file);
-    }
+  // tree and branches
+  TFile* rootfile = new TFile("rootfile.root","RECREATE");
+  TTree* tree = new TTree("HitTree", "Hit Tree");
+  // tree->Branch("nhits", &nhits, "nhits/I");
+  tree->Branch("euEvt", &bcid, "bcid/I");
+  tree->Branch("lv1", &l1id, "l1id/I");
+  tree->Branch("iden", &ONE, "l1id/I");  // TODO: read this for real
+  // tree->Branch("tag", &l1id, "tag/I");
+  tree->Branch("col", &col);
+  tree->Branch("row", &row);
+  tree->Branch("tot", &tot);
+  
+  std::fstream file(fn, std::fstream::in | std::fstream::binary);
+
+  file.seekg(0, std::ios_base::end);
+  int size = file.tellg();
+  file.seekg(0, std::ios_base::beg);
+  std::cout << "Size: " << size/1024.0/1024.0 << " MB" << std::endl;
+
+  while(file) {
+    eventFromFileBinary(file);
+    tree.Fill()
+  }
+
+  tree->Write();
+  rootfile->Close();
 }
